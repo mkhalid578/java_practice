82911c9c54d3f25585a0b5bdbbf20e62f0fed6f3
mkhalid578 <muhammed_khalid@student.uml.edu>
2017-07-20 11:38:37 -0400

    Hello World

---
+++ Introductions.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+    
+public class Introductions {
+
+    public static void main(String[] args) {
+	Console console = System.console();
+	console.printf("Hello World!\n");
+    }
+}
