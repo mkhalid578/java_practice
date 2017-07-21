457a9a596e23527f33667a7c92bf12dd42e63ab5
mkhalid578 <muhammed_khalid@student.uml.edu>
2017-07-21 09:25:38 -0400

    more java

--- Introductions.java
+++ Introductions.java
@@ -3,7 +3,12 @@ import java.io.Console;
 public class Introductions {
 
     public static void main(String[] args) {
+	// Variables
+	String firstName = "Muhammed"; // camelCasing
 	Console console = System.console();
-	console.printf("Hello World!\n");
+	console.printf("Hello, my name is %s\n", firstName);
+	// Request input
+	String lastName = console.readLine("What is your name?  ");
+	console.printf("%s\n", lastName);
     }
 }
