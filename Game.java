197a55097ee68f9d85fb86c5ff4528bfce9632ed
Muhammed Khalid <mkhalid@Uzairs-MacBook-Air.local>
2017-07-29 22:40:06 -0400

    prompt for guess

--- Game.java
+++ Game.java
@@ -1,7 +1,21 @@
 class Game {
     private String answer;
-
+    private String misses;
+    private String hits;
     public Game(String answer) {
 	this.answer = answer;
+	hits = "";
+	misses = "";
+    }
+
+    public boolean applyGuess(char letter) {
+	boolean isHit = answer.indexOf(letter) != -1;
+	if (isHit) {
+	    hits += letter;
+	} else {
+	    misses += letter;
+	}
+
+	return isHit;
     }
 }
--- Hangman.java
+++ Hangman.java
@@ -2,5 +2,13 @@ public class Hangman {
 
     public static void main (String [] args) {
 	// Your code begins here
+	Game game = new Game("treehouse");
+	Prompter prompter = new Prompter(game);
+	boolean isHit = prompter.promptForGuess();
+	if (isHit) {
+	    System.out.println("We got a hit!");
+	} else {
+	    System.out.println("ooops missed");
+	}
     }
 }
